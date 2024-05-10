2c49c0260c71e542d74d234185c09618252e0456
m1_air <m1_air@m1-airs-Air.hsd1.co.comcast.net>
2024-05-10 12:46:11 -0600

    basic swift project files

---
+++ swiftuicomponents/swiftuicomponents/swiftuicomponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuicomponentsApp.swift
+//  swiftuicomponents
+//
+//  Created by m1_air on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftuicomponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
